f342179542309fe2d51db4e9bb7dbd156ff613ee
Bradley G Smith <bradley.g.smith@gmail.com>
2021-03-28 12:26:10 -0700

    add configuration options

--- container-example/base-image-build.sh
+++ container-example/base-image-build.sh
@@ -6,7 +6,31 @@ set -x
 # repository and creates an external mount point for /srv/repodir
 # that can be used with a -v switch in podman/docker
 
-repodir=/srv/repodir
+# custom image name
+custom_name=myfedora
+
+# scratch conf file name
+tmp_name=local.conf
+
+# location of plugin config file
+configuration_name=/etc/dnf/plugins/local.conf
+
+# location of repodir on container
+container_repodir=/srv/repodir
+
+# create scratch plugin conf file for container
+# using repodir location as set in container_repodir
+cat <<EOF > "$tmp_name"
+[main]
+enabled = true
+repodir = $container_repodir
+[createrepo]
+enabled = true
+# If you want to speedup createrepo with the --cachedir option. Eg.
+# cachedir = /tmp/createrepo-local-plugin-cachedir
+# quiet = true
+# verbose = false
+EOF
 
 # pull registry.fedoraproject.org/fedora:latest
 podman pull registry.fedoraproject.org/fedora:latest
@@ -23,17 +47,19 @@ buildah run "$mkdev" -- dnf --nodocs -y install python3-dnf-plugin-local creater
 buildah run "$mkdev" -- dnf -y clean all
 
 # create the repo dir
-buildah run "$mkdev" -- mkdir -p "$repodir"
+buildah run "$mkdev" -- mkdir -p "$container_repodir"
 
-# copy the dnf-local-plugin conf file from host - assumes repodir on host
-# matches repodir above
-buildah copy "$mkdev" "/etc/dnf/plugins/local.conf" "/etc/dnf/plugins/local.conf"
+# copy the scratch plugin conf file from host
+buildah copy "$mkdev" "$tmp_name" "$configuration_name"
 
-# mark repodir as a mount point for host volume
-buildah config --volume "$repodir" "$mkdev"
+# mark container repodir as a mount point for host volume
+buildah config --volume "$container_repodir" "$mkdev"
 
 # create myfedora image
-buildah commit "$mkdev" "localhost/myfedora:latest"
+buildah commit "$mkdev" "localhost/$custom_name:latest"
 
 # clean up working image
 buildah rm "$mkdev"
+
+# remove scratch file
+rm $tmp_name
